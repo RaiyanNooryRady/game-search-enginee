dc0147239baa7988e1944bed44605bb85af5daa3
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-06 15:27:14 +0600

    children component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 interface TestProps{
   text:string;
   color:string;
+  children?: React.ReactNode;
 }
 function App() {
   return (
@@ -10,18 +11,23 @@ function App() {
       <Test text="Hello World" color="red"/>
       <Test text="Hello World 2" color="blue"/>
       <Test text="Hello World 3" color="green"/>
-      <Test text="Hello World 4" color="yellow"/>
+      <Test text="Hello World 4" color="yellow">
+        <h2>This is a yellow test</h2>
+      </Test>
       <Test text="Hello World 5" color="purple"/>
       <Test text="Hello World 6" color="orange"/>
       <Test text="Hello World 7" color="red"/>
-      <Test text="Hello World 8" color="brown"/>
+      <Test text="Hello World 8" color="brown"> 
+        <h2>This is a brown test</h2>
+      </Test>
     </>
   )
 }
-function Test({text,color}:TestProps){
+function Test({text,color,children}:TestProps){
   return(
     <div>
       <h1 style={{color:color}}>{text}</h1>
+      {children}
     </div>
   );
 }
