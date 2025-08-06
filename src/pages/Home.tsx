07777d4d2f2e73023ebf733814b6db687b1d6fb1
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-06 18:03:56 +0600

    preventing default load on submit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,21 @@
 import games from '../assets/games'
 import GameCard from '../components/GameCard'
+import { useState } from 'react'
 const Home = () => {
 
+    const [searchQuery, setSearchQuery] = useState("");
+    const handleSearch = (e: React.FormEvent) => {
+        e.preventDefault();
+        alert(searchQuery);
+    }
 
     return (
         <div className="home">
             <h1 className="home-title">Games</h1>
+            <form onSubmit={handleSearch} className='search-form' action="">
+                <input type="text" placeholder='Search games' className='search-input' value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
+                <button type='submit' className='search-button'>Search</button>
+            </form>
             <div className="games-grid">
                 {
                     games.map((game) => {
