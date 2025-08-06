631d4f0bab89664ff9e29a0f75dcf6e204596d30
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-06 15:21:24 +0600

    props and destructuring with type annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import './App.css'
 
+interface TestProps{
+  text:string;
+  color:string;
+}
 function App() {
   return (
     <>
-      <Test text="Hello World"/>
-      <Test text="Hello World 2"/>
-      <Test text="Hello World 3"/>
-      <Test text="Hello World 4"/>
-      <Test text="Hello World 5"/>
-      <Test text="Hello World 6"/>
-      <Test text="Hello World 7"/>
-      <Test text="Hello World 8"/>
+      <Test text="Hello World" color="red"/>
+      <Test text="Hello World 2" color="blue"/>
+      <Test text="Hello World 3" color="green"/>
+      <Test text="Hello World 4" color="yellow"/>
+      <Test text="Hello World 5" color="purple"/>
+      <Test text="Hello World 6" color="orange"/>
+      <Test text="Hello World 7" color="red"/>
+      <Test text="Hello World 8" color="brown"/>
     </>
   )
 }
-function Test(props:any){
+function Test({text,color}:TestProps){
   return(
     <div>
-      <h1>{props.text}</h1>
+      <h1 style={{color:color}}>{text}</h1>
     </div>
   );
 }
