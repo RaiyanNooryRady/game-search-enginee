0813871677ac0147068631544d525ff1571471b7
raiyannooryrady <raiyannooryrady@gmail.com>
2025-08-06 15:12:05 +0600

    props any added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-danger'>Hello World</h1>
+      <Test text="Hello World"/>
+      <Test text="Hello World 2"/>
+      <Test text="Hello World 3"/>
+      <Test text="Hello World 4"/>
+      <Test text="Hello World 5"/>
+      <Test text="Hello World 6"/>
+      <Test text="Hello World 7"/>
+      <Test text="Hello World 8"/>
     </>
   )
+}
+function Test(props:any){
+  return(
+    <div>
+      <h1>{props.text}</h1>
+    </div>
+  );
 }
 
 export default App
